a6a4dc9c9d0694dc17b5c428cd84e0bcc2a54334
Sourabh <sourabhraj311@gmail.com>
2024-08-07 22:53:41 +0530

    Fixed height and width of homescreen

--- src/components/common/PrimaryCardIcon.tsx
+++ src/components/common/PrimaryCardIcon.tsx
@@ -1,11 +1,34 @@
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import homeStyles from '../../styles/components/HomeStyles'
 import GlobalStyles from '../../styles/general/global_styles'
+import { SCREEN_WIDTH } from '@gorhom/bottom-sheet'
 
 const PrimaryCardIcon = ({
     text, icon, onPress
 }: any) => {
+    const styles = StyleSheet.create({
+        card: {
+            width: SCREEN_WIDTH * 0.40,
+            height: SCREEN_WIDTH * 0.40,
+            borderRadius: 20,
+            backgroundColor: GlobalStyles.cardBgColor,
+            margin: 12
+        },
+        cardText: {
+            marginTop: 24,
+            marginStart: 16,
+            fontFamily: GlobalStyles.baseFont,
+            fontSize: 16,
+            color: GlobalStyles.grey700
+        },
+        cardIconContainer: {
+            margin: 16
+        },
+        cardIcon: {
+            height: 55,
+            width: 55
+        }
+    })
     return (
         <TouchableOpacity activeOpacity={0.8} style={styles.card} onPress={onPress}>
             <Text style={styles.cardText}>{text}</Text>
@@ -17,27 +40,3 @@ const PrimaryCardIcon = ({
 }
 
 export default PrimaryCardIcon
-
-const styles = StyleSheet.create({
-    card: {
-        width: 163,
-        height: 139,
-        borderRadius: 20,
-        backgroundColor: GlobalStyles.cardBgColor,
-        margin: 12
-    },
-    cardText: {
-        marginTop: 24,
-        marginStart: 16,
-        fontFamily: GlobalStyles.baseFont,
-        fontSize: 16,
-        color: GlobalStyles.grey700
-    },
-    cardIconContainer: {
-        margin: 16
-    },
-    cardIcon: {
-        height: 55,
-        width: 55
-    }
-})
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,17 +1,97 @@
-import { Image, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, Platform, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, useWindowDimensions, View } from 'react-native'
 import React from 'react'
 import HeadingTextsHome from '../../components/common/HeadingTextsHome'
 import PrimaryCardIcon from '../../components/common/PrimaryCardIcon'
-import homeStyles from '../../styles/components/HomeStyles'
 import { HomeTabParamList } from './Home'
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
 import { useNavigation } from '@react-navigation/native'
 import { RootState } from '../../redux/reducers/user/userStore'
 import { useSelector } from 'react-redux'
+import GlobalStyles from '../../styles/general/global_styles'
+import { SCREEN_WIDTH } from '@gorhom/bottom-sheet'
 
+const isIos = Platform.OS === 'ios';
 const HomeScreen = () => {
     const navigation = useNavigation<BottomTabNavigationProp<HomeTabParamList>>();
     const user = useSelector((state: RootState) => state.user);
+    const homeStyles = StyleSheet.create({
+        container: {
+            alignItems: 'center',
+            backgroundColor: GlobalStyles.pagBackground,
+            flexDirection: 'column',
+            flexGrow: 1,
+            width: SCREEN_WIDTH,
+        },
+        margin: {
+            flex: 1,
+            marginTop: isIos ? 70 : 50,
+        },
+        topContainer: {
+            height: 200,
+            width: '100%',
+        },
+        header: {
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0,
+            zIndex: -1,
+            height: 300,
+            minWidth: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            opacity: 0.9,
+            width: '100%',
+        },
+        image: {
+            height: '100%',
+            width: '100%',
+            resizeMode: 'cover',
+        },
+        iconContainer: {
+            width: '88%',
+            marginHorizontal: 24,
+            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+            marginTop: 8,
+            flexDirection: 'row',
+        },
+        iconButton: {
+            height: 50,
+            width: 50,
+        },
+        icon: {
+            height: 50,
+            width: 50,
+            resizeMode: 'cover',
+        },
+        body: {
+            position: 'relative',
+            borderTopLeftRadius: 40,
+            top: -40, //-240,
+            color: 'black',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: useWindowDimensions().width,
+            height: '100%',
+            marginBottom: -40,
+            marginTop: 100,
+            paddingTop: 35,
+            paddingHorizontal: 16,
+            backgroundColor: GlobalStyles.pagBackground,
+        },
+        horizontal: {
+            flexDirection: 'row',
+        },
+        marginNegetative: {
+            flex: 1,
+            flexGrow: 1,
+            height: 30,
+            width: '100%',
+            // marginTop: 100
+        }
+    });
     
     return (
         <View style={homeStyles.container}>
@@ -33,7 +113,7 @@ const HomeScreen = () => {
                 <HeadingTextsHome text1={'Hey' + ' ' + ((user.user?.first_name) ? user.user?.first_name.substring(0, 15) : "User")} text2='Welcome to health.io' />
             </View>
             <View style={homeStyles.body}>
-                <View style={homeStyles.marginNegetative}>
+                <ScrollView showsVerticalScrollIndicator={false} style={homeStyles.marginNegetative}>
                     <View style={homeStyles.horizontal}>
                         <PrimaryCardIcon
                             icon={require('../../assets/icons/hospital.png')}
@@ -55,10 +135,12 @@ const HomeScreen = () => {
                             onPress={() => { }}
                             text='Laboratory' />
                     </View>
-                </View>
+                </ScrollView>
             </View>
         </View>
     )
 }
+
+
 
 export default HomeScreen
